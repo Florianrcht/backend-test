10842295fab47f25ecb4ff06ffea472d0daf8336
Florian <113524385+Florianrcht@users.noreply.github.com>
2025-07-17 20:59:01 +0200

    database config

---
+++ database_actions/migrations/3_create_characteristics_table.up.sql
@@ -0,0 +1,4 @@
+CREATE TABLE characteristics (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    characteristic VARCHAR(50) NOT NULL
+);
---
+++ database_actions/migrations/4_create_breeds_characteristics_relation_table.down.sql
@@ -0,0 +1,1 @@
+DROP TABLE IF EXISTS breeds_characteristics_relation;
